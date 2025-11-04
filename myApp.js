83634e4fab6e6bcb26612020c2e749d67e37442b
OmPrakash <iewd1285@gmail.com>
2025-11-04 14:58:56 +0530

    start a working express server

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
 
+/**
+ * 1. Meet the Node console
+ */
 console.log("Hello World");
+/**
+ * 2. Start a Working Express Server
+ */
+app.get("/", function(req, res){
+    res.send("Hello Express");
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const myApp = require('./myApp');
-const express = require('express');
-const app = express();
 
 // Use environment variable for port or default to 3000
 const port = process.env.PORT || 3000;
 
 // Start the server
-app.listen(port, () => {
+myApp.listen(port, () => {
   console.log(`Node is listening on port ${port}...`);
   console.log(`Access it at http://localhost:${port}`);
 });
