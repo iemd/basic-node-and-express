c6547efac5c7e049897cdd5903b777e959a11cee
OmPrakash <iewd1285@gmail.com>
2025-11-04 15:06:03 +0530

    serve an html file

--- myApp.js
+++ myApp.js
@@ -8,8 +8,14 @@ console.log("Hello World");
 /**
  * 2. Start a Working Express Server
  */
-app.get("/", function(req, res){
-    res.send("Hello Express");
+//app.get("/", function (req, res) {
+//    res.send("Hello Express");
+//});
+/**
+ * 3. Serve an HTML file
+ */
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
